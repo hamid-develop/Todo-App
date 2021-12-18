b9d27aba4801875db53ca26614f53eafa28635f9
hamid-develop <h.alizadeh87@hotmail.com>
2021-12-18 12:15:57 +0330

    todo app updated...

--- todo-app/src/components/AddTodo/AddTodo.js
+++ todo-app/src/components/AddTodo/AddTodo.js
@@ -1,28 +1,29 @@
 import { useState, useContext } from "react";
 import { TodoContext } from "../../context/TodoContextProvider";
-import './AddTodo.css'
-import {Input, Button} from 'antd'
+import "./AddTodo.css";
+import { Input, Button } from "antd";
 
 const AddTodo = () => {
   const [inputValue, setInputValue] = useState("");
   const { addTodoHandler } = useContext(TodoContext);
   return (
     <div className="addTodo">
-      <Input
-      type="text"
-      placeholder="please enter your todo item.."
-      value={inputValue}
-      onChange={(e) => setInputValue(e.target.value)} />
-      
-      <Button
-      type="primary"
-        onClick={() => {
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
           addTodoHandler(inputValue);
           setInputValue("");
         }}
       >
-        Add todo
-      </Button>
+        <Input
+          type="text"
+          placeholder="please enter your todo item.."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+
+        <Button type="submit">Add todo</Button>
+      </form>
     </div>
   );
 };
